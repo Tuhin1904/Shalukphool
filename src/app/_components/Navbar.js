922cd4e9fbbec6dc5156a2fffb59654e50d88738
Tuhin1904 <iamtuhinn@gmail.com>
2025-01-11 21:07:15 +0530

    Post dishes correction

--- src/app/_components/Navbar.js
+++ src/app/_components/Navbar.js
@@ -193,11 +193,11 @@ export default function Navbar() {
               "Item 3",
               "Item 4",
               "Item 5",
-              "Item 1",
-              "Item 2",
-              "Item 3",
-              "Item 4",
-              "Item 5",
+              "Item 6",
+              "Item 7",
+              "Item 8",
+              "Item 9",
+              "Item 10",
             ].map((item, index) => (
               <li
                 key={index}
@@ -222,11 +222,11 @@ export default function Navbar() {
                   "Item 3",
                   "Item 4",
                   "Item 5",
-                  "Item 1",
-                  "Item 2",
-                  "Item 3",
-                  "Item 4",
-                  "Item 5",
+                  "Item 6",
+                  "Item 7",
+                  "Item 8",
+                  "Item 9",
+                  "Item 10",
                 ][hoveredItem]
               }`}</p>
             </div>
